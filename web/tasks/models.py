376a75ee5e570970c77085115b4ed9e43ae30cf2
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2025-11-20 23:21:14 +0000

    Añade campo urgent en el modelo Task

--- web/tasks/models.py
+++ web/tasks/models.py
@@ -7,6 +7,7 @@ class Task(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     slug = models.SlugField(unique=True)
+    urgent = models.BooleanField(default=False)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
